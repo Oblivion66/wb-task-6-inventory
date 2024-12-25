a5592619fa8be8347f8c6c0e4532ef553978484b
unknown <myradusmanov@gmail.com>
2024-12-25 03:58:26 +0300

    почти реализовал, поправил баги, осталось выделить цвета для редкости

--- src/components/InventoryItem/InventoryItem.tsx
+++ src/components/InventoryItem/InventoryItem.tsx
@@ -1,7 +1,7 @@
 import { InventoryItemProps } from "./InventoryItem.types";
 
 export const InventoryItem = ({ item, inventoryWidth }: InventoryItemProps) => {
-  const { category, rarity, width, height } = item;
+  const { category, width, height } = item;
   const cell = 600 / inventoryWidth - 2;
 
   return (
@@ -10,17 +10,19 @@ export const InventoryItem = ({ item, inventoryWidth }: InventoryItemProps) => {
       style={{
         gridColumn: `span ${width}`,
         gridRow: `span ${height}`,
+        position: "relative",
+        zIndex: 20,
       }}>
       <img
-      className="inventory-item__image"
+        className="inventory-item__image"
         src={`/images/${category}2.png`}
         alt={category}
         style={{
           position: "absolute",
           top: 0,
           left: 0,
-          width: `${width * cell}px`,
-          height: `${height * cell}px`,
+          width: `${width * cell - 10 }px`,
+          height: `${height * cell - 10}px`,
           objectFit: "cover",
         }}
       />
